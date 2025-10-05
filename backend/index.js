4b99d744e3e3d00a9b930a445edd8fac73a1c2a0
Muskan Rathee <muskanrathee725@gmail.com>
2025-10-05 09:08:22 +0530

    Connect to MongoDB Atlas database

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const app = express();
+require('dotenv').config();
+const connectDB = require('./config/db');
+const { connect } = require('mongoose');
+
+connectDB();
 
 
 
